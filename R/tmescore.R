e18b747b665afb5df62ed918deaed1192812fd50
DongqiangZeng0808 <708902023@qq.com>
2019-06-26 02:00:14 +0800

    update functions

--- R/tmescore.R
+++ R/tmescore.R
@@ -1,5 +1,31 @@
 
 
+
+
+
+
+
+##' Calculate score across genes and samples
+##'
+##' This wrapper function combines filtering out genes with low reads in a number of samples (recommended for limma:voom) with normalization
+##' @param gm normalized count matrix; rows are all genes in the signature that shall be summarized into one score; columns are samples
+##' @param summarizationFunction character vector defining whether signature scores shall be based on principal component 1 ("PC", default) or z-scores (other value)
+##' @return numeric vector of length ncol(gm); a score summarizing the rows of gm
+##' @author Dorothee Nickles
+##' @export
+gsScore <- function(gm, summarizationFunction="PC") {
+  if (summarizationFunction == "PC") {
+    pc <- prcomp(t(gm),
+                 retx=TRUE)
+    gss <- pc$x[,1] * sign(cor(pc$x[,1], colMeans(gm)))
+  } else {
+    gss <- colMeans(gm)
+  }
+  return(gss)
+}
+
+
+
 #' calculate TMEscore
 #'
 #' @param eset normalized expression data use function: scale(); with human gene symbol in rows and patients ID in columns;
