204a3fdbb090c1270c9bedc20ab0d8fbb609fd62
DongqiangZeng0808 <dongqiangzeng0808@gmail.com>
2020-12-21 16:24:59 +0800

    Update tmescore.R

--- R/tmescore.R
+++ R/tmescore.R
@@ -17,7 +17,7 @@
 #' @examples
 #'
 best_cutoff<-function(pdata,variable,time = "time",status = "status",PrintResult = T){
-  
+
   colnames(pdata)[which(colnames(pdata)==time)]<-"time"
   colnames(pdata)[which(colnames(pdata)==status)]<-"status"
   y<-Surv(pdata$time,pdata$status)
@@ -50,7 +50,7 @@ best_cutoff<-function(pdata,variable,time = "time",status = "status",PrintResult
 ##' @author Dorothee Nickles, Dongqiang Zeng
 ##' @export
 sigScore <- function(eset, methods = "PCA") {
-  
+
   if (methods == "PCA") {
     # message(paste0("Calculating siganture score using PCA function"))
     pc <- prcomp(t(eset))
@@ -68,7 +68,7 @@ sigScore <- function(eset, methods = "PCA") {
 #' Calculate TMEscore
 #'
 #' @param eset normalized expression data use function: scale(); with human gene symbol in rows and patient identifier in columns;
-#' @param pdata phenotype data with patient identifier or other clinical data in columns; 
+#' @param pdata phenotype data with patient identifier or other clinical data in columns;
 #' the number of rows in pdata should be equal to the number of columns in eset
 #' @param method  use PCA (default) or z-score function to calculate TMEscore
 #' @param print_gene_pro print signature gene propotion, default is FALSE
@@ -79,23 +79,23 @@ sigScore <- function(eset, methods = "PCA") {
 #' @export
 #' @author Dongqiang Zeng
 #' @examples
-#' 
+#'
 #' tmescore<-tmescore(eset = eset_stad,pdata = pdata_stad,column_of_sample = "ID")
 #'
 
 tmescore<-function(eset, pdata = NULL, column_of_sample = "ID",method = "PCA",print_gene_pro = FALSE, classify = FALSE){
-  
-  
+
+
   #to prevent crashing on duplicated gene symbols, add unique numbers to identical names
   dups <- dim(eset)[1] - length(unique(rownames(eset)))
   if(dups > 0) {
-    warning(paste(dups," duplicated gene symbol(s) found in mixture file! \n 
+    warning(paste(dups," duplicated gene symbol(s) found in mixture file! \n
                   Expression set will be order by the mean value, and genes with the highest mean value will be preserved",sep=""))
     eset<-eset[order(apply(eset,1,mean), decreasing = T),]
     eset<-eset[!duplicated(rownames(eset)),]
   }
   ###################################
-  
+
   #check genes in expression set
   ###################################
   freq1<-length(intersect(signature$TMEscoreA,rownames(eset)))/length(signature$TMEscoreA)
@@ -114,31 +114,31 @@ tmescore<-function(eset, pdata = NULL, column_of_sample = "ID",method = "PCA",pr
     message(paste0(paste0(sprintf("  %1.2f%%", 100*freq2)," of TMEscoreB signature genes appear on gene matrix")))
   }
   ##################################
-  
+
   #filter signatures
   mini_gene_count <- 2 # count of overlapping genes should more than 2
   tme_signature<-signature[c("TMEscoreA","TMEscoreB")]
   tme_signature<-tme_signature[lapply(tme_signature,function(x) sum(x%in%rownames(eset)==TRUE))>= mini_gene_count]
   #################################
-  
-  
+
+
   if(!"TMEscoreA"%in% names(tme_signature) & !"TMEscoreB"%in% names(tme_signature)){
     stop("TMEscore signature genes did not appear on expression matirx ")
   } else if("TMEscoreA"%in% names(tme_signature) & !"TMEscoreB"%in% names(tme_signature)){
     warning("Only TMEscoreA will be estimated for insufficient TMEscoreB signature genes appear on gene matrix")
   } else if(!"TMEscoreA"%in% names(tme_signature) & "TMEscoreB"%in% names(tme_signature)){
     warning("Only TMEscoreB will be estimated for insufficient TMEscoreA signature genes appear on gene matrix")
-  } 
+  }
   #################################
   #creat pdata if not provided
   if(is.null(pdata)){
     pdata<-data.frame("Index" = 1:length(colnames(eset)),"ID" = colnames(eset))
     ###############################
-    if(!dim(eset)[2]==dim(pdata)[1]) 
-      message("Expression set data (eset) and phenotype data (pdata) have different number of samples, 
+    if(!dim(eset)[2]==dim(pdata)[1])
+      message("Expression set data (eset) and phenotype data (pdata) have different number of samples,
             \n only samples with expression set provided will be estimated")
   }
-  
+
   #match phenotype data and gene expression set
   ###########################
   colnames(pdata)[which(colnames(pdata)==column_of_sample)]<-"ID"
@@ -146,10 +146,10 @@ tmescore<-function(eset, pdata = NULL, column_of_sample = "ID",method = "PCA",pr
   eset<-eset[,colnames(eset)%in%pdata$ID]
   eset<-eset[,match(pdata$ID,colnames(eset))]
   ###############################
-   
+
   # if(max(eset)<50 & array) eset<- preprocessCore::normalize.quantiles(eset)
-  
-  if(max(eset)>100) eset<-log2(eset+0.01)
+
+  if(max(eset)>100) eset<-log2(eset+1)
   ###############################
   goi <- names(tme_signature)
   for (sig in goi) {
@@ -167,11 +167,11 @@ tmescore<-function(eset, pdata = NULL, column_of_sample = "ID",method = "PCA",pr
   if(classify & !c("time"%in%colnames(pdata) & "status"%in%colnames(pdata)) ) {
    message("If survival data are provided, please transform column names of follow up data into 'time' and 'status' ")
   }
-  
+
   if(classify & "time"%in%colnames(pdata) &"status"%in%colnames(pdata)) {
     pdata<-best_cutoff(pdata = pdata, variable = "TMEscore",time = "time",status = "status",PrintResult = F)
   }
-  
+
   return(pdata)
 }
 
